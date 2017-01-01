063a73d9100818f7189ee7e3e650d868b5567bbc
depippo <jdepippo@gmail.com>
2017-01-01 17:13:35 -0500

    Done.

--- tests/FavoriteFood.spec.js
+++ tests/FavoriteFood.spec.js
@@ -7,5 +7,22 @@ describe('Favorite Food Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
+  it('should correctly filter the array by favorite food', function() {
+    var list = [{
+      name: "Jim",
+      favoriteFood: "Tacos"
+    },{
+      name: "Dorothy",
+      favoriteFood: "Burritos"
+    },{
+      name: "Hugh",
+      favoriteFood: "Quesadillas"
+    }];
+
+    var results = $filter('favoriteFood')(list, 'Tacos');
+
+    expect(results.length).toBe(1);
+    expect(results[0].name).toBe('Jim');
+  });
 	
 });
--- tests/removeAllVowels.spec.js
+++ tests/removeAllVowels.spec.js
@@ -7,4 +7,12 @@ describe('removeAllVowels Filter', function () {
 		$filter = $injector.get('$filter');
 	}));
 
+  it('should remove all the vowels from a given string', function() {
+    var string = "pack my box with five dozen liquor jugs";
+
+    var results = $filter('removeAllVowels')(string);
+
+    expect(results).toBe('pck my bx wth fv dzn lqr jgs');
+  })
+
 });
